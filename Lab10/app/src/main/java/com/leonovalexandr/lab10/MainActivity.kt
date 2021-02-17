d58860ea17b5c342d7ddeaf88db963c56bbf8f87
Леонов А.В <leonov22.a.v@gmail.com>
2021-02-17 23:51:37 +0700

    Заготовка для 10 лабы

--- Lab10/app/src/main/java/com/leonovalexandr/lab10/MainActivity.kt
+++ Lab10/app/src/main/java/com/leonovalexandr/lab10/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.leonovalexandr.lab10
 
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.view.View
+import android.widget.TextView
+
+const val BROADCAST_TIME_EVENT = "com.leonovalexandr.lab10.timeevent"
 
 class MainActivity : AppCompatActivity() {
+
+    var receiver: BroadcastReceiver? = null
+    val filter = IntentFilter(BROADCAST_TIME_EVENT)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        receiver = object : BroadcastReceiver() {
+            // Получено широковещательное сообщение
+            override fun onReceive(context: Context?, intent: Intent) {
+                val counter = intent.getIntExtra("counter", 0)
+                val textCounter = findViewById<TextView>(R.id.counter_view)
+                textCounter.text = counter.toString()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun buttonStartService(view: View) {
+        startService(Intent(this, TimeService::class.java))
+        bindService(Intent(this, TimeService::class.java), myConnection, Context.BIND_AUTO_CREATE)
+        registerReceiver(receiver, filter)
+    }
+
+    fun buttonStopService(view: View) {
+        unregisterReceiver(receiver)
+        unbindService(myConnection)
+        stopService(Intent(this, TimeService::class.java))
+    }
+
+    fun buttonGetCounter(view: View){
+        if(isBound)
+            findViewById<TextView>(R.id.counter_view).text = myService!!.getCounter().toString()
+    }
+
+    var myService: TimeService? = null
+    var isBound = false
+    val myConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as TimeService.MyBinder
+            myService = binder.getService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            isBound = false
+        }
     }
 }
--- Lab10/app/src/main/java/com/leonovalexandr/lab10/TimeService.kt
+++ Lab10/app/src/main/java/com/leonovalexandr/lab10/TimeService.kt
@@ -2,18 +2,52 @@ package com.leonovalexandr.lab10
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimeService : Service() {
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    inner class MyBinder: Binder() {
+        fun getService() : TimeService {
+            return this@TimeService
+        }
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int { return super.onStartCommand(intent, flags, startId)
+    private var counter = 0
+    private lateinit var job: Job
+
+    private val myBinder = MyBinder()
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return myBinder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        job = GlobalScope.launch {
+            while (true) {
+                delay(1000)
+                Log.d("SERVICE", "Timer Is Ticking: " + counter)
+                counter++
+                val intent = Intent(BROADCAST_TIME_EVENT)
+                intent.putExtra("counter", counter)
+                sendBroadcast(intent);
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
+        Log.d("SERVICE", "onDestroy")
+        job.cancel()
         super.onDestroy()
+    }
+
+    fun getCounter(): Int {
+        return counter
     }
 }
